17f6c2f33d301a5d635c533d08556f63c1161ce1
Monica Howard <mo@Monicas-Air.attlocal.net>
2021-02-08 15:20:17 -0600

    Added Form component to App

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,28 +1,21 @@
 // import logo from './logo.svg';
-import {useState} from 'react';
-
-import logo from './tek_logo.png';
+import React, { useContext, useReducer } from 'react';
 import './App.css';
-import Todo from './Components/Todo';
+import TodoList from './Pages/TodoList'
+import TodoForm from './Components/TodoForm'
+import TodosContext from './context'
+import todosReducer from './reducer'
 
 function App() {
-
-  const [todos, setTodos] = useState(["eat","sleep","code","pray","repeat"]);  //sample todo items.  these will need to be changed in your app.  Just filler todos, although they are quite important!
+  const initialState = useContext(TodosContext)
+  const [state, dispatch] = useReducer(todosReducer, initialState)
+  
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-         <br/>
-          <p>Create a Todo List App</p>
-          <Todo item="1"/>
-          <Todo item="2"/>
-          <Todo item="3"/>
-          {todos.map( (t,i) => <Todo key={i} item={t}/>)}
-        </div>
-      </header>
-    </div>
+    <TodosContext.Provider value={{state, dispatch}}>
+      <TodoForm />
+      <TodoList />
+    </TodosContext.Provider>
   );
 }
 
